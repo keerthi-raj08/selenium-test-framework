5971afcec98c772c50b4679cd21e979afddc667b
keerthi-raj08 <151744256+keerthi-raj08@users.noreply.github.com>
2025-07-28 11:56:03 +0530

    Create BaseTest.java

---
+++ src/test/java/tests/src/test/java/utils/BaseTest.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://example.com"); // Replace with your test URL
+    }
+
+    @AfterMethod
+   () {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
