08cfd2b271415ff8ee1fbaee2833ebb8bfcb5511
keerthi-raj08 <151744256+keerthi-raj08@users.noreply.github.com>
2025-07-28 11:50:48 +0530

    Create LoginTest.java

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void testLogin() {
+        System.out.println("Login test executed");
+    }
+}
+    
