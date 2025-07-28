c15df1588ced46318fa92704677d11f7e7bafa25
keerthi-raj08 <151744256+keerthi-raj08@users.noreply.github.com>
2025-07-28 12:07:20 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,18 +1,1 @@
-package tests;
 
-import org.testng.annotations.Test;
-import pages.LoginPage;
-import utils.BaseTest;
-
-public class LoginTest extends BaseTest {
-
-    @Test
-    public void testLogin() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.enterUsername("testuser");
-        loginPage.enterPassword("testpass");
-        loginPage.clickLogin();
-
-        System.out.println("Login test executed using Page Object Model");
-    }
-}
