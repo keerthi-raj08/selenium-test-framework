f569767c16620354a1d86aa36199f82151d84aa9
keerthi-raj08 <151744256+keerthi-raj08@users.noreply.github.com>
2025-07-28 12:34:00 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,28 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-    WebDriver driver;
+    private WebDriver driver;
 
-    // Locators
-    By usernameField = By.id("username");
-    By passwordField = By.id("password");
-    By loginButton = By.id("login");
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login");
 
-    // Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Actions
-    public void enterUsername(String username) {
+    public void login(String username, String password) {
         driver.findElement(usernameField).sendKeys(username);
-    }
-
-    public void enterPassword(String password) {
         driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
     }
 
-    public void clickLogin() {
-        driver.findElement(loginButton).click();
+    public boolean isLoginSuccessful() {
+        return driver.getCurrentUrl().contains("dashboard");
     }
 }
